7b7a879c9b775b399571585d23a575cf6123656c
Sandra Rodriguez <rodriguezsandra.com>
2023-06-24 21:12:43 -0500

    agregarJs

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 
 console.log('¡Hola prueba!');
+
+const opciones = document.querySelectorAll('.opciones');
+console.log('opciones');
+
+opciones.forEach(function (opciones) {
+    opciones.addEventListener("click", function (e) {
+        const currentopciones = document.querySelector(".bnav");
+        currentopciones.classList.remove("bnav");
+        e.target.classList.add("bnav");
+    
+    
+
+    
+ });
+
+});
+
+const body = document.querySelector("body");
+const botn= document.querySelector("#boton");
+
+botn.addEventListener("click", function (e) {
+    body.classList.toggle("Modo_Oscuro");
+    
+ 
+
+});
